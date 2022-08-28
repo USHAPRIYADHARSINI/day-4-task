406c0d4041805344df959c4dad4aee16eaaf459c
USHAPRIYADHARSINI <110903183+USHAPRIYADHARSINI@users.noreply.github.com>
2022-08-28 22:55:41 +0530

    Add files via upload

---
+++ day 4 task.js
@@ -0,0 +1,41 @@
+// comparing 2 json have the same properties with order
+// comparing the objects
+var obj1=
+{
+    name:"Person1",
+    age:"5"
+};
+var obj2=
+{
+    age:"5",
+    name :"Person1"
+};
+console.log(obj1===obj2); 
+console.log(obj1==obj2);
+
+// comparing the properties
+console.log(obj1.name===obj2.name);
+console.log(obj1.age===obj2.age);
+
+// comparing without order
+
+var flag=true;
+
+if(Object.keys(obj1).length===Object.keys(obj2).length){
+    for(key in obj1) { 
+        if(obj1[key] == obj2[key]) {
+            continue;
+        }
+        else {
+            flag=false;
+            break;
+        }
+    }
+}
+else {
+    flag=false;
+}
+console.log("Both the objects have the "+flag +" properties");
+
+
+
